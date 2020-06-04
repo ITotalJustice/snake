94aff8a105ef8105d4252bec88a46ea0dd0f20bc
TotalJustice <smithtest741@gmail.com>
2020-06-04 12:33:39 +0100

    remove argc/v handling (for now)

--- source/main.c
+++ source/main.c
@@ -1,38 +1,9 @@
 #include "includes.h"
 #include "snake.h"
 
-#define STRMATCH(x,y) ((strcmp(x,y) == 0))
-
 int main(int argc, char *argv[])
 {
     fprintf(stdout, "hello world\n");
-    
-    /// handle cmd args.
-    if (argc > 1)
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            /// load save
-            if (STRMATCH("--loadsave", argv[i]))
-            {
-                /// check that we have a string after load.
-                if (i + 1 != argc)
-                {
-                    ++i;
-                    fprintf(stdout, "loading save: %s\n", argv[i]);
-                }
-                else
-                {
-                    fprintf(stderr, "no arg given after using %s\n", argv[i]);
-                }
-            }
-
-            else if (STRMATCH("--norend", argv[i]))
-            {
-
-            }
-        }
-    }
 
     snake_play();
     
